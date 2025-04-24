7a081a41c69bc0316497d3d560f3ace5e7edbb42
Moon Han Young <sorhf12@naver.com>
2025-04-24 14:45:40 +0900

    도전문제

--- jajva_useful/src/_string/StringBufferTest1.java
+++ jajva_useful/src/_string/StringBufferTest1.java
@@ -19,5 +19,14 @@ public class StringBufferTest1 {
         System.out.println("수정 후 :" + System.identityHashCode(buffer1));
 
 
+        //API 8버전에서는 buffer1.toString() 해야 값을 확인할 수 있다.
+                                      // 이전버전 : 해시코드
+        System.out.println(buffer1);  // 출력값 : HelloWorld
+
+        //StringBuffer 데이터 타입을 String 으로 형 변환 하려면 .toString() 호출하면 됨.
+        String result1 = buffer1.toString();
+
+
+
     } // end of main
 } // end of class
